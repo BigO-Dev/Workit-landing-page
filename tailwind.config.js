cee040c8a2ef1e39f8ed34e8b27eed24bcbb712c
Omair Majid <omajid.dev@gmail.com>
2024-06-18 14:53:50 -0700

    chore: Update .gitignore and index.html

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Work Sans"', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-pattern': "url('./assets/images/background-pattern-desktop.svg)",
+      },
+      fontSize: {
+        '6xl': '3.5rem',
+      },
+    },
+  },
+}
